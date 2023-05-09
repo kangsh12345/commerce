a8d252753ef7c26e4662adbcbcf18aad912e926a
ksh <kangshqwert@naver.com>
2023-05-09 22:19:59 +0900

    [기능추가]: 스키마에 생성시간 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,9 +15,9 @@ export default function Home() {
   //     .then(res => res.json())
   //     .then(data => setProducts(data.items));
   // }, []);
-  const [products, setProducts] = useState<Array<{ id: string; name: string }>>(
-    [],
-  );
+  const [products, setProducts] = useState<
+    Array<{ id: string; name: string; createdAt: string }>
+  >([]);
   useEffect(() => {
     fetch('/api/get-products')
       .then(res => res.json())
@@ -74,7 +74,12 @@ export default function Home() {
                 </div>
               ))} */}
             {products &&
-              products.map(item => <div key={item.id}>{item.name}</div>)}
+              products.map(item => (
+                <div key={item.id}>
+                  {item.name}
+                  <span>{item.createdAt}</span>
+                </div>
+              ))}
           </div>
         </div>
       </main>
