7b5eb5d672291389dcf59423da6e6e981ce17bb2
ksh <kangshqwert@naver.com>
2023-05-10 13:08:31 +0900

    [기능추가]: TailWindCSS 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,7 +46,12 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div>
-          <input ref={inputRef} type="text" placeholder="name" />
+          <input
+            className="placeholder:italic placeholder:text-slate-400 block bg-white w-96 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+            ref={inputRef}
+            type="text"
+            placeholder="name"
+          />
           <button onClick={handleClick}>Add Jacket</button>
           <div>
             <p>Product List</p>
