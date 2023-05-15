19397225503d733a469f28b6d854c282c5c0f242
ksh <kangshqwert@naver.com>
2023-05-15 13:17:16 +0900

    [기능추가]: 캐러셀 썸네일 띄우기

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,5 +1,6 @@
 // import ImageGallery from 'react-image-gallery';
 
+import { useState } from 'react';
 import Image from 'next/image';
 import Carousel from 'nuka-carousel';
 
@@ -16,22 +17,76 @@ const images = [
     original: 'https://picsum.photos/id/1019/1000/600/',
     thumbnail: 'https://picsum.photos/id/1019/250/150/',
   },
+  {
+    original:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/4v.jpg',
+    thumbnail:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/4v.jpg',
+  },
+  {
+    original:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/3.jpg',
+    thumbnail:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/3.jpg',
+  },
+  {
+    original:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/2.jpg',
+    thumbnail:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/2.jpg',
+  },
+  {
+    original:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/1.jpg',
+    thumbnail:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/1.jpg',
+  },
 ];
 
 export default function Products() {
+  const [index, setIndex] = useState(0);
+
   // return <ImageGallery items={images} />;
   return (
-    <Carousel animation="fade" autoplay withoutControls wrapAround speed={10}>
-      {images.map(item => (
-        <Image
-          key={item.original}
-          src={item.original}
-          alt="image"
-          width={1000}
-          height={600}
-          style={{ width: '100%', height: 'auto' }}
-        />
-      ))}
-    </Carousel>
+    <>
+      <Carousel
+        animation="fade"
+        autoplay
+        withoutControls
+        wrapAround
+        // speed={10}
+        autoplayInterval={3000}
+        slideIndex={index}
+      >
+        {images.map(item => (
+          <Image
+            key={item.original}
+            src={item.original}
+            alt="image"
+            width={1000}
+            height={600}
+            style={{ width: '100%', height: '100%' }}
+          />
+        ))}
+      </Carousel>
+      <div style={{ display: 'flex' }}>
+        {images.map((item, idx) => (
+          <div
+            key={idx}
+            onClick={() => {
+              setIndex(idx);
+            }}
+          >
+            <Image
+              src={item.original}
+              alt="image"
+              width={100}
+              height={60}
+              style={{ width: '100%', height: '100%' }}
+            />
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
