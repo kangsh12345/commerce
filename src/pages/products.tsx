e1d622ccbc2ac8717c429c8a3a680434674f6c1f
ksh <kangshqwert@naver.com>
2023-05-15 14:24:47 +0900

    [기능추가]: next-sitemap 추가

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,6 +1,7 @@
 // import ImageGallery from 'react-image-gallery';
 
 import { useState } from 'react';
+import Head from 'next/head';
 import Image from 'next/image';
 import Carousel from 'nuka-carousel';
 
@@ -49,6 +50,25 @@ export default function Products() {
   // return <ImageGallery items={images} />;
   return (
     <>
+      <Head>
+        <meta
+          property="og:url"
+          content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html"
+        />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:title"
+          content="When Great Minds Don’t Think Alike"
+        />
+        <meta
+          property="og:description"
+          content="How much does culture influence creative thinking?"
+        />
+        <meta
+          property="og:image"
+          content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
+        />
+      </Head>
       <Carousel
         animation="fade"
         autoplay
