8fb60e101903311f35e743d00c77fbfa59ea6b28
ksh <kangshqwert@naver.com>
2023-05-19 16:25:01 +0900

    [기능추가]: 상품벼별 카테고리 데이터 분류, 필터링

--- src/pages/api/get-products.ts
+++ src/pages/api/get-products.ts
@@ -3,12 +3,25 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-async function getProducts(skip: number, take: number) {
+async function getProducts(skip: number, take: number, category: number) {
+  const where =
+    category && category !== -1
+      ? {
+          where: {
+            category_id: category,
+          },
+        }
+      : undefined;
+
   try {
     const response = await prisma.products.findMany({
       // 몇개를 스킵하고 몇개를 가져올지
       skip: skip,
       take: take,
+      ...where,
+      orderBy: {
+        price: 'asc',
+      },
     });
     console.log(response);
     return response;
@@ -26,13 +39,17 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  const { skip, take } = req.query;
+  const { skip, take, category } = req.query;
   if (skip === null || take === null) {
     res.status(400).json({ message: 'no skip or take' });
   }
 
   try {
-    const products = await getProducts(Number(skip), Number(take));
+    const products = await getProducts(
+      Number(skip),
+      Number(take),
+      Number(category),
+    );
     res.status(200).json({ items: products, message: `Success` });
   } catch (error) {
     res.status(400).json({ message: `Failed` });
--- src/pages/products/page.tsx
+++ src/pages/products/page.tsx
@@ -1,67 +1,88 @@
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
-import { Pagination } from '@mantine/core';
-import { products } from '@prisma/client';
+import { Pagination, SegmentedControl } from '@mantine/core';
+import { categories, products } from '@prisma/client';
 
 import { CATEGORY_MAP, TAKE } from '~/constants/products';
 
 export default function Products() {
   const [products, setProducts] = useState<products[]>([]);
   const [total, setTotal] = useState(0);
+  const [categories, setCategories] = useState<categories[]>([]);
   const [activePage, setPage] = useState(1);
+  const [selectedCategory, setSelectedCategory] = useState<string>('-1');
 
   useEffect(() => {
-    fetch(`/api/get-products-count`)
+    fetch(`/api/get-categories`)
       .then(res => res.json())
-      .then(data => setTotal(Math.ceil(data.items / TAKE)));
-    fetch(`/api/get-products?skip=0&take=${TAKE}`)
-      .then(res => res.json())
-      .then(data => setProducts(data.items));
+      .then(data => setCategories(data.items));
   }, []);
+
+  useEffect(() => {
+    fetch(`/api/get-products-count?category=${selectedCategory}`)
+      .then(res => res.json())
+      .then(data => {
+        setPage(1);
+        setTotal(Math.ceil(data.items / TAKE));
+      });
+  }, [selectedCategory]);
 
   useEffect(() => {
     const skip = TAKE * (activePage - 1);
 
-    fetch(`/api/get-products?skip=${skip}&take=${TAKE}`)
+    fetch(
+      `/api/get-products?skip=${skip}&take=${TAKE}&category=${selectedCategory}`,
+    )
       .then(res => res.json())
       .then(data => setProducts(data.items));
-  }, [activePage]);
+  }, [activePage, selectedCategory]);
 
   return (
     <div className="px-36 mt-36">
+      {categories && (
+        <div className="mb-4">
+          <SegmentedControl
+            value={selectedCategory}
+            onChange={setSelectedCategory}
+            data={[
+              { label: 'ALL', value: '-1' },
+              ...categories.map(category => ({
+                label: category.name,
+                value: String(category.id),
+              })),
+            ]}
+            color="dark"
+          />
+        </div>
+      )}
       {products && (
+        // <div className="flex flex-wrap justify-center">
         <div className="grid grid-cols-3 gap-5">
           {products.map(item => (
-            <div key={item.id} className="flex flex-col justify-between">
-              <Image
-                className="rounded"
-                src={item.image_url ?? ''}
-                alt={item.name}
-                width={300}
-                height={200}
-                style={{ width: '100%' }}
-                placeholder="blur"
-                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mO8WA8AAicBUm5VYI8AAAAASUVORK5CYII="
-              />
-              <div>
-                <div className="flex">
-                  <span>{item.name}</span>
-                  <span className="ml-auto">
-                    {item.price.toLocaleString('ko-KR')}원
+            <div key={item.id}>
+              <div style={{ width: 310 }}>
+                <div style={{ position: 'relative', width: 310, height: 390 }}>
+                  <Image
+                    className="rounded"
+                    src={item.image_url ?? ''}
+                    alt={item.name}
+                    fill
+                    style={{ objectFit: 'cover', overflow: 'hidden' }}
+                    placeholder="blur"
+                    blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mO8WA8AAicBUm5VYI8AAAAASUVORK5CYII="
+                  />
+                </div>
+                <div>
+                  <div className="flex">
+                    <span>{item.name}</span>
+                    <span className="ml-auto">
+                      {item.price.toLocaleString('ko-KR')}원
+                    </span>
+                  </div>
+                  <span className="text-zinc-400">
+                    {CATEGORY_MAP[item.category_id - 1]}
                   </span>
                 </div>
-                <span className="text-zinc-400">
-                  {/* {item.category_id === 1
-                    ? '신발'
-                    : item.category_id === 2
-                    ? '티셔츠'
-                    : item.category_id === 3
-                    ? '바지'
-                    : item.category_id === 4
-                    ? '모자'
-                    : '후드티'} */}
-                  {CATEGORY_MAP[item.category_id]}
-                </span>
               </div>
             </div>
           ))}
